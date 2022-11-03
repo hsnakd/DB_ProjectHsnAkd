5189a786c77093de933878fdd7f1f29f902281a0
hsnakd <hsnakd10@gmail.com>
2022-11-04 01:40:52 +0300

    DAY7 FINISHED ASSIGNMENT

--- MySql/VyTrack2.sql
+++ MySql/VyTrack2.sql
@@ -1,6 +1,20 @@
 SELECT *
 FROM orocrm_contact;
 
-SELECT ID, first_name, last_name
+SELECT *
+FROM orocrm_contact_email;
+
+SELECT id, first_name, last_name
 FROM orocrm_contact
-WHERE first_name = 
+WHERE first_name = 'Mister';
+
+SELECT *
+FROM orocrm_contact_email
+WHERE owner_id = 20;
+
+-- write the query to get first_name, last_name, email for Mister
+SELECT first_name, last_name, orocrm_contact_email.email
+FROM orocrm_contact INNER JOIN orocrm_contact_email
+ON orocrm_contact.id = orocrm_contact_email.owner_id
+WHERE first_name = 'Mister';
+
